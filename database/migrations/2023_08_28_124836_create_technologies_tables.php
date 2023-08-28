ce8e563e1bbc1fa0efa761dbe055a73343271860
Oxidra <dantelezzo@gmail.com>
2023-08-28 14:56:13 +0200

    changed migration table name

--- database/migrations/2023_08_28_124836_create_technologies_tables.php
+++ database/migrations/2023_08_28_124836_create_technologies_tables.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('technologies_tables', function (Blueprint $table) {
+        Schema::create('technologies', function (Blueprint $table) {
             $table->id();
             $table->string("name");
             $table->timestamps();
