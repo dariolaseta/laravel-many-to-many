fca016c5d5304718576fcd00efe2885213def140
Dario La Seta <126085129+dariolaseta@users.noreply.github.com>
2023-08-28 14:42:59 +0200

    Initial commit

---
+++ app/Models/Admin/Project.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models\Admin;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Project extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "title",
+        "image",
+        "content"
+    ];
+
+    public function Types(){
+        return $this->belongsTo(Project::class);
+    }
+}
